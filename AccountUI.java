384116bb1eb94eeac5c69c80dbcc35ceb0b22b4c
DALVAREZ001 <dalvarez001@vplincsci050.ad.csbsju.edu>
2020-02-23 18:15:36 -0600

    accountUI, University, Universitycontroller

---
+++ AccountUI.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package University;
+
+/**
+ * @author dalvarez001
+ *
+ */
+public interface AccountUI {
+	 
+	
+	public static void logOn(String username, String password) {
+		
+	}
+	
+	public static void logOut() {
+		
+	}
+
+	public static void viewResults() { 
+		
+	}
+}

76973f170408962dd79c003c4050e1ed3cb5517d
Joseph Laux <43391267+jlaux001@users.noreply.github.com>
2020-02-25 00:29:16 -0600

    Add files via upload

---
+++ AdminUI.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * @author jlaux001
+ *
+ */
+public class AdminUI {
+	
+	public void addSchool(String university) {
+		
+	}
+	public void editSchool() {
+		
+	}
+	public void logout() {
+		
+	}
+}

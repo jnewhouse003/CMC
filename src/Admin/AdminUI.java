fd571013ad53ea25390542ec4c74415a2eed3b86
NPINK001 <npink001@vplincsci034.ad.csbsju.edu>
2020-02-25 21:36:49 -0600

    University folder deleted and moved some packages

--- src/Admin/AdminUI.java
+++ src/Admin/AdminUI.java
@@ -1,3 +1,4 @@
+package Admin;
 
 
 /**
---
+++ src/Driver/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Driver;
+
+/**
+ * @author npink001
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

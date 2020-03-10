ef42561a27de8aa666863152a5d63695373a7111
NPINK001 <npink001@vplincsci078.ad.csbsju.edu>
2020-03-09 22:29:31 -0500

    Added comments to the other Account classes

--- src/Account/AccountController.java
+++ src/Account/AccountController.java
@@ -1,13 +1,11 @@
-/**
- * 
- */
 package Account;
 
 import DB.DBController;
 
 /**
+ * This is the controller class for Accounts
+ * 
  * @author npink001
- *
  */
 public class AccountController {
 
@@ -16,6 +14,12 @@ public class AccountController {
 		
 	}
 	
+	/**
+	 * This method logs an account on
+	 * 
+	 * @param username
+	 * @param password
+	 */
 	public void logOn(String username, String password) {
 		DBController dbController = new DBController("goldencircle","csci230");
 		if (dbController.lookUpUser(username) == true)
@@ -39,6 +43,11 @@ public class AccountController {
 		
 	}
 	
+	/**
+	 * This method logs an Account out
+	 * 
+	 * @param account
+	 */
 	public void logOut(String account) {
 		DB.DBController dbController = new DB.DBController("goldencircle","csci230");
 		
@@ -68,6 +77,14 @@ public class AccountController {
 		System.out.println("Last Name: " + user.getLastName()); 
 	}
 	
+	/**
+	 * This method changes the information tied to a user's Account
+	 * 
+	 * @param info
+	 * @param newPassword
+	 * @param newFirstName
+	 * @param newLastName
+	 */
 	public void editUserInfo(Account info, String newPassword, String newFirstName, String newLastName) {
 		info.setPassword(newPassword); 
 		info.setFirstName(newFirstName);
@@ -75,6 +92,16 @@ public class AccountController {
 		
 	}
 
+	/**
+	 * This method adds a new User Account
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param userName
+	 * @param password
+	 * @param type
+	 * @return
+	 */
 	public int addUser(String firstName, String lastName, String userName, String password, char type) {
 		// TODO Auto-generated method stub
 		DBController dbController = new DBController("goldencircle","csci230");
--- src/Account/AccountUI.java
+++ src/Account/AccountUI.java
@@ -8,39 +8,60 @@ import java.util.ArrayList;
 import university.University;
 
 /**
+ * This is the interface for an AccountUI
+ * 
  * @author dalvarez001
- *
  */
 public interface AccountUI {
-	 
-	
+
+	/**
+	 * This method calls on the controller to set an Account to logged on 
+	 * 
+	 * @param username
+	 * @param password
+	 */
 	public static void logOn(String username, String password) {
-		
+
 		AccountController name = new AccountController();
 		name.logOn(username, password);
-		
-		
+
 	}
-	
+
+	/**
+	 * This method calls on the controller to set an Account to logged out
+	 * 
+	 * @param account
+	 */
 	public static void logOut(String account) {
-			AccountController name = new AccountController();
-			name.logOut(account);
+		AccountController name = new AccountController();
+		name.logOut(account);
 	}
 
-	public static void viewResults() { 
-		
+	public static void viewResults() {
+
 	}
 
+	/**
+	 * This method calls on the controller to add a User Account
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param userName
+	 * @param password
+	 * @param type
+	 * @return an integer to be used by the database
+	 */
 	public static int addUser(String firstName, String lastName, String userName, String password, char type) {
 		// TODO Auto-generated method stub
 		AccountController name = new AccountController();
 		return name.addUser(firstName, lastName, userName, password, type);
-		
+
 	}
-	
-	
-	
-	
-	
-	
+
 }
+
+
+
+
+
+

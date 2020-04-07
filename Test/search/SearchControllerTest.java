da7237a3bbe356f987db82001ceb34f95b543f87
JNEWHOUSE003 <jnewhouse003@vplincsci029.ad.csbsju.edu>
2020-04-07 13:43:22 -0500

    DBController

---
+++ Test/search/SearchControllerTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package search;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author giteghete001
+ *
+ */
+public class SearchControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

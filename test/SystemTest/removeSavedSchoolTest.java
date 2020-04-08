fe8c0d319176df63a86b74a7de78e914cbbdb060
JLIPETZKY001 <jlipetzky001@vplincsci035.ad.csbsju.edu>
2020-04-08 13:41:26 -0500

    removeSavedSchoolTest

---
+++ test/SystemTest/removeSavedSchoolTest.java
@@ -0,0 +1,43 @@
+package SystemTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import Account.AccountUI;
+import Admin.AdminUI;
+import DB.DBController;
+import user.UserUI;
+
+
+public class removeSavedSchoolTest {
+	
+	DBController dataBase;
+	
+	@Before
+	public void setUp() throws Exception{
+		this.dataBase = new DBController("goldencircle", "csci230";)
+		UserUI.createController(dataBase);
+		dataBase.addUser(test, tester, tester, 123, s);
+		dataBase.addSavedSchool(tester, Harvard)
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		dataBase.removeUser("tester");
+	}
+	
+	@Test
+	public void testRemoveSavedSchools() {
+		user.removeSavedSchool(dataBase.getUniversity(Harvard));
+		Assert.assertTrue("Harvard should not be in saved schools list",return dataBase.getSavedSchool(tester););
+		
+		
+		
+	}
+
+}

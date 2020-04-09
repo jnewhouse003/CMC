f0c4c5215846f12832620ff75073fcb735839f10
DALVAREZ001 <dalvarez001@vplincsci034.ad.csbsju.edu>
2020-04-09 14:33:46 -0500

    Search

--- Test/search/SearchControllerTest.java
+++ Test/search/SearchControllerTest.java
@@ -6,32 +6,65 @@ package search;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import DB.DBController;
+import university.University;
+import user.User;
+import user.UserUI;
+import search.searchController;
 
 /**
  * @author giteghete001
  *
  */
 public class SearchControllerTest {
+	searchController controller;
+	DBController dataBase;
+	
+
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		//creating the universities
+		University uni1 = new University("Circle University", "Minnesota", "location1", "control", 53, 40.5, 50.5, 1000, 2000, 2000.00, 1500.00, 100, 30.00, 50.00, 98, 23, 80, "majors");
+		//University uni2 = new University("peterUni", "Chicago", 1000);
+		//University uni3 = new University("ohmannUni", "Texas", 1200);
+		
+		this.controller = new searchController();
+		this.dataBase = new DBController("goldencircle", "cs230");
+		
+		//Adding universities to DB
+		this.dataBase.addUser("test", "mcTesty", "testy", "123", 'u');
+		this.dataBase.addSchool("Circle University");
+		//this.dataBase.addSchool("peterUni"));
+		//this.dataBase.addSchool("ohmannUni"));
+		
+		
+		
 	}
+	
+	//userControllerTest, 
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@After
 	public void tearDown() throws Exception {
+		
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSearchSchool() {
+		Assert.assertTrue("Circle University was found", this.controller.searchSchool("Minnesota", 53) == true);
+		
+		Assert.assertTrue("University Not Found", this.controller.searchSchool("fdb", 1000)== false);
 	}
 
 }
--- test/SystemTest/removeSavedSchoolTest.java
+++ test/SystemTest/removeSavedSchoolTest.java
@@ -2,6 +2,7 @@ package SystemTest;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,10 +20,10 @@ public class removeSavedSchoolTest {
 	
 	@Before
 	public void setUp() throws Exception{
-		this.dataBase = new DBController("goldencircle", "csci230";)
+		this.dataBase = new DBController("goldencircle", "csci230");
 		UserUI.createController(dataBase);
-		dataBase.addUser("test", "tester", "tester", "123", "s");
-		dataBase.addSavedSchool(tester, Harvard)
+		dataBase.addUser("test", "tester", "tester", "123", 's');
+		dataBase.addSavedSchool("tester", "Harvard");
 		
 	}
 	
@@ -33,7 +34,7 @@ public class removeSavedSchoolTest {
 	
 	@Test
 	public void testRemoveSavedSchools() {
-		Assert.assertEquals("Removed School", true, userUI.removeSavedSchool("tester", "Harvard"));
+		Assert.assertEquals("Removed School", true, UserUI.removeSavedSchool("tester", "Harvard"));
 		
 		
 	}

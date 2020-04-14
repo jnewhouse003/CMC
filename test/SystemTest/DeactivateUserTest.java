d853a70a4f3d8200ea8a709f3f40c0a156128bb0
GITEGHETE001 <giteghete001@vplincsci042.ad.csbsju.edu>
2020-04-14 16:41:40 -0500

    additions to deactivate user test

--- test/SystemTest/DeactivateUserTest.java
+++ test/SystemTest/DeactivateUserTest.java
@@ -9,17 +9,27 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import Account.AccountUI;
+import Admin.AdminUI;
+import DB.DBController;
+
 /**
  * @author giteghete001
  *
  */
 public class DeactivateUserTest {
+	DBController database;
+	
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		this.database = new DBController("goldencircle", "csci230");
+		AccountUI.createController(database);
+		database.addUser("rice", "m", "mrice001", "password1", "u");
+		AdminUI.activateUser("mrice001");
 	}
 
 	/**
@@ -31,7 +41,7 @@ public class DeactivateUserTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }

9de023c3aec9ebc131b6796e9281536a72d8d636
GITEGHETE001 <giteghete001@vplincsci042.ad.csbsju.edu>
2020-04-14 22:58:09 -0500

    Final addition to the Java documentation in the source file

--- src/university/UniversityController.java
+++ src/university/UniversityController.java
@@ -11,26 +11,45 @@ public class UniversityController {
 
 	
 	//how will it take in the criteria
+	
+	/**
+	 * This method is used to search schools given criteria  
+	 * @return University
+	 */
 	public University searchSchool(Criteria) {
 		
 		return University;
 	}
 	
-	
+	/**
+	 * This method is used to add university to list of schools 
+	 */
 	public void addSchool(University) {
+		 
 	
 		
 	}
-	
+	/**
+	 * This method is used to edit the school information
+	 */
 	public void editSchool(University) {
-		
+		return null;
 	}
 	
+	/**
+	 * This method is used to get university given the same name 
+	 * @param name
+	 * @return null 
+	 */
 	public University getUniversity(String name) {
 		
 		return null;
 	}
 	
+	/**
+	 *  This method is used to get name of the school
+	 * @return null
+	 */
 	public String getName(University) {
 		
 		return null;
